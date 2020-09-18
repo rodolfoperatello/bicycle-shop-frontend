7d98f492b2b952ed135f2b6190b46db2defdca8b
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-09-18 16:02:14 -0300

    Creating modules for product-api

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+import { getProducts } from "./mocks/products.js";
+
+(function () {
+  const productsElement = document.querySelector("#product-list");
+
+  let productsElements = "";
+
+  getProducts().forEach((products) => {
+    productsElements += `
+      <li class="card">
+        <img
+          class="card__image"
+          src="${products.imageSrc}"
+          alt="Imagem ${products.name}"
+        />
+  
+        <div class="card__info">
+          <p class="card__info__text">${products.name}
+            <span class="display--block font-size--1">${products.price}</span>
+          </p>
+  
+          <button class="button">Adicionar ao Carrinho</button>
+        </div>
+        
+      </li>
+      `;
+  });
+
+  productsElement.innerHTML = productsElements;
+})();

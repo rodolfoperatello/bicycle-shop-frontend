3760a402788d6103026e117f7f93850aa56771c3
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-09-18 16:19:02 -0300

    Fixing module for products-API

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,27 @@
 import { getProducts } from "./mocks/products.js";
 
-(function () {
-  const productsElement = document.querySelector("#product-list");
+const productsElement = document.querySelector("#product-list");
 
-  let productsElements = "";
+let productsElements = "";
 
-  getProducts().forEach((products) => {
-    productsElements += `
-      <li class="card">
-        <img
-          class="card__image"
-          src="${products.imageSrc}"
-          alt="Imagem ${products.name}"
-        />
-  
-        <div class="card__info">
-          <p class="card__info__text">${products.name}
-            <span class="display--block font-size--1">${products.price}</span>
-          </p>
-  
-          <button class="button">Adicionar ao Carrinho</button>
-        </div>
-        
-      </li>
-      `;
-  });
+getProducts().forEach((products) => {
+  productsElements += `
+    <li class="card">
+      <img
+        class="card__image"
+        src="${products.imageSrc}"
+        alt="Imagem ${products.name}"
+      />
 
-  productsElement.innerHTML = productsElements;
-})();
+      <div class="card__info">
+        <p class="card__info__text">${products.name}
+          <span class="display--block font-size--1">${products.price}</span>
+        </p>
+
+        <button class="button">Adicionar ao Carrinho</button>
+      </div>
+    </li>
+  `;
+});
+
+productsElement.innerHTML = productsElements;
